8f12cc924ad39208acb3b142a7335f37bb531744
curtaincall <curtaincall@cmsedu.co.kr>
2018-12-13 13:45:31 +0900

    switch backend url

--- frontend/src/apollo/apolloClient.js
+++ frontend/src/apollo/apolloClient.js
@@ -1,7 +1,8 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000"
+  //uri: "http://localhost:4000"
+  uri: "http://spacegallery-dev4us-backend.surge.sh"
 });
 
 export default client;
