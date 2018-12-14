4dbb88539f90281e85a03f2a6ef83011462b56fb
curtaincall <curtaincall@cmsedu.co.kr>
2018-12-14 18:12:27 +0900

    set frontend part deploy on now.sh and deploy

--- frontend/src/component/Navigator/Navigator.js
+++ frontend/src/component/Navigator/Navigator.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import styles from './Navigator.scss';
-import classNames from 'classnames/bind';
+import React from "react";
+import styles from "./Navigator.scss";
+import classNames from "classnames/bind";
 
-import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 const cx = classNames.bind(styles);
 
-const Navigator = ({handleDate}) => {
+const Navigator = ({ handleDate }) => {
   return (
-    <div className={cx('navigator')}>
-      <div onClick={() => handleDate('prev')}>
-        <MdChevronLeft className={cx('arrow')}/>
+    <div className={cx("navigator")}>
+      <div onClick={() => handleDate("prev")}>
+        <MdChevronLeft className={cx("arrow")} />
       </div>
-      <div onClick={() => handleDate('next')}>
-        <MdChevronRight className={cx('arrow')}/>
+      <div onClick={() => handleDate("next")}>
+        <MdChevronRight className={cx("arrow")} />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navigator;
