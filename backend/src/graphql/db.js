12f7c599f83c2a6696ac830756ed6eb1ec40be00
curtaincall <curtaincall@cmsedu.co.kr>
2018-12-17 18:17:31 +0900

    use axios on API Request without node-fetch

--- backend/src/graphql/db.js
+++ backend/src/graphql/db.js
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+import axios from "axios";
 
 const API_PARAM_KEY = "TP9y8C8u6U7gj4roR0AS9ePBqsA6d0FcpCa6Dahu";
 const API_PARAM_URL = "https://api.nasa.gov/planetary/apod?api_key=";
@@ -7,5 +7,5 @@ export const getSpacePic = (date = "") => {
   let API_PARAM_DATE = "&date=" + date;
   const API_END_POINT = API_PARAM_URL + API_PARAM_KEY + API_PARAM_DATE;
 
-  return fetch(`${API_END_POINT}`).then(res => res.json());
+  return axios.get(API_END_POINT);
 };
--- frontend/src/apollo/apolloClient.js
+++ frontend/src/apollo/apolloClient.js
@@ -2,7 +2,7 @@ import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
   //uri: "http://localhost:4000"
-  uri: "https://spacegallery-backend-ivsedzl30.now.sh/"
+  uri: "https://spacegallery-backend-ohcb7sftw.now.sh"
 });
 
 export default client;
